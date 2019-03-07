01ca81a77692129ad212292073981a820bab6039
FicheeSS <40142995+FicheeSS@users.noreply.github.com>
2019-03-07 16:54:47 +0100

    Add files via upload
    initial

---
+++ couleur.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# definitions de quelques couleurs ... par synthèse additive des sous pixels 
+# rouge, vert et bleu.: les intesnsités des sous-pixels varient entre 0 et 255.
+# il y a 256 valeurs différentes car chaque intensité est codée sur 8 bits
+# (2^8 = 256)
+
+rouge = [255,0,0]
+vert = [0,255,0]
+bleu = [0,0,255]
+jaune = [255,255,0]
+magenta = [255,0,255]
+cyan = [0,255,255]
+blanc = [255,255,255]
+orange = [255,127,0]
+noir = [0,0,0]
---
+++ main.py
@@ -0,0 +1,53 @@
+'''
+Author : Théo Bocquet
+'''
+import pygame 
+from tools import *
+from levels import *
+from couleur import *
+import numpy
+
+#Le but ici va etre de faire tourner le coeur du programme 
+#global const
+posBouleStartup = (50,50)
+bouleRaduis = 15
+widthCase = 25
+heightCase = 15
+posPalletStartup = (posBouleStartup[0]-bouleRaduis,posBouleStartup[1]-bouleRaduis)
+screenSize = (40*15,40*25)
+
+#global var
+currentBoulePos = posBouleStartup
+level = 1
+
+def main():
+    #le programme princpal se fait apres le init 
+    print("oui")
+
+
+
+
+
+
+def dessineCarre(pos , couleur):
+    pygame.draw.rect(screen,couleur,(pos[0],pos[1],widthCase,heightCase))
+def getPosFromTab(tab):
+    pos = (tab[0]*screenSize[0],tab[1]*screenSize[1])
+    return pos
+
+def init ():
+    #a faire au debut pour positioner les cases et la boule
+    pygame.init()
+    # Création et affichage de la fenêtre graphique de largeur 900 et hauteur 600
+    screen = pygame.display.set_mode(screenSize)
+    pygame.display.flip()
+    x = 0
+    y = 0
+    while y < 15:
+        while x < 20 :
+            listTemp = list(niveau1[x,y])
+            dessineCarre(getPosFromTab(listTemp))
+            x += 1
+        y+= 1 
+
+init ()
