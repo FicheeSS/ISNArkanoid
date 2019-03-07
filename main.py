cf6ae5fd52e773e76436b63d299c0bb59ee1fe9b
FicheeSS <theo.ange.bocquet@gmail.com>
2019-03-07 17:06:04 +0100

    t

--- main.py
+++ main.py
@@ -32,6 +32,11 @@ def main():
 def dessineCarre(pos , couleur):
     pygame.draw.rect(screen,couleur,(pos[0],pos[1],widthCase,heightCase))
 def getPosFromTab(tab):
+    """
+    renvoi la position du relative par rapport en tableau
+    tab : positon dans le tableau
+    return : la position 
+    """
     pos = (tab[0]*screenSize[0],tab[1]*screenSize[1])
     return pos
 
