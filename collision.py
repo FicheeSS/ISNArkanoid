a503dd04fc38470c6f2dc455482b271c199b938f
FicheeSS <40142995+FicheeSS@users.noreply.github.com>
2019-03-25 14:51:02 +0100

    Add files via upload

--- collision.py
+++ collision.py
@@ -8,11 +8,24 @@ def DetectColisionCercleDroite (p1,p2,c,ray):
     A=1+a
     B = -2*c[0]+2*a*b-2*a*c[1]
     C = c[0]*c[0]+c[1]*c[1]+b*b-2*b*c[1]-ray*ray
+    print(B*B-4*A*C)
     if B*B-4*A*C < 0 :
         #il se passe rien pas de point d'intersection
         return []
     else:
         #il a point d'intersection
-        return [(p1,-B-math.sqrt(B*B-4*A*C)/2*A),(p2,-B+math.sqrt(B*B-4*A*C)/2*A)]
+        if appartenance(p1,p2,[((-B-math.sqrt(B*B-4*A*C)/2*A),A*(-B-math.sqrt(B*B-4*A*C)/2*A)+B),((-B+math.sqrt(B*B-4*A*C)/2*A),A*(-B+math.sqrt(B*B-4*A*C)/2*A)+B)]) == True :    
+            return [((-B-math.sqrt(B*B-4*A*C)/2*A),A*(-B-math.sqrt(B*B-4*A*C)/2*A)+B),((-B+math.sqrt(B*B-4*A*C)/2*A),A*(-B+math.sqrt(B*B-4*A*C)/2*A)+B)]
+        else :
+            return [0]
 
+def appartenance(p1,p2,intersec):
+    
+    if intersec[0][0] >= p1[0] and intersec[0][0] <= p2[0]:
+        return True
+    elif intersec[1][0] >= p1[0] and intersec[1][0] <= p2[0]:
+        return True
+    else :
+        return False
+    
 
