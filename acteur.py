bcfc05eba8026c578a6f477a271e07b01eaceb7c
FicheeSS <40142995+FicheeSS@users.noreply.github.com>
2019-04-08 14:21:28 +0200

    Palette

--- acteur.py
+++ acteur.py
@@ -1,10 +1,8 @@
 from globalvar import *
-from palette import *
 import pygame
 import math
 import random
 from collision import *
-from mur import *
 from couleur import *
 #réglage des paramètres de balle : vitesse, rebond
 class Balle:
@@ -118,3 +116,23 @@ class Mur:
         return self.extremites
     def get_type(self):
         return self.type
+        
+class Palette:
+    def __init__(self):
+        self.height = 10
+        self.width = 50
+        self.x = int(screenSize[0]/2 - self.width/2)
+        self.y = screenSize[1]- self.height - 2 
+        self.mvtdelta = 10
+    def dessine(self , screen):
+        pygame.draw.rect(screen,blanc,(self.x,self.y,self.width,self.height))
+    def animate(self):
+        pygame.event.pump()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    if self.x < screenSize[0]:
+                        self.x += self.mvtdelta
+                if event.key == pygame.K_LEFT:
+                    if self.x > 0 :
+                        self.x -= self.mvtdelta
